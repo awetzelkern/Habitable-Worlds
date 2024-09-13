4c861f2dd8f85c3a33f023f3b121d452978cffc7
Andrew Kern <awetzelkern@gmail.com>
2024-09-13 13:43:20 -0500

    Finished Importing
    Need to fix errors for graphs to show.

--- graph.py
+++ graph.py
@@ -66,10 +66,131 @@ import matplotlib.colors as cl
 import numpy as np
 import pandas as pd
 
-data=pd.read_csv(('fwOly9Fl.csv'),header=[0],skiprows=[1])
+data=pd.read_csv(('planet_data.csv'),header=[0],skiprows=[1])
 data_v2=data.dropna()
 data_v3=data_v2.drop(data_v2[data.MASS<0].index)
 
 data_v3
 print(data_v3)
 Mj = 1.898e27
+
+# loop and print results
+MsSI = 1.989e30
+TransitDepth_List = []
+TransitProb_List = []
+Mplanet = []
+Mhost = []
+HabitablePlanets = []
+for i in range(len(data_v3["NAME"])):
+  Mh = data.iloc[i]["MSTAR"]*MsSI
+  if Mh<1.66*MsSI:
+    Rh=1.06*Rs*pow(Mh/MsSI,0.945)
+  else:
+    Rh=1.33*Rs*pow(Mh/MsSI,0.555)
+
+  Mp = data_v3.iloc[i]["MASS"]*1.898e27
+  Ap = data_v3.iloc[i]["A"]*1.496e+11
+  Ep = data_v3.iloc[i]["ECC"]
+  Rp = pow(Mp/MsSI,0.485)*Rs
+  TransitDepth_List.append(TransitDepth(Rp,Rs))
+  TransitProb_List.append(TransitProb(Rh,Ap,Ep))
+  Mplanet.append(Mp/9.223e18)
+  Mhost.append(Mh/9.223e18)
+  
+plt.scatter(data_v3["MSTAR"],data_v3["MASS"],alpha=.4,s=13)
+plt.xlabel("Host Star Mass (M☉)",size=15)
+plt.ylabel("Exo-Planet Mass (M♃)",size=15)
+plt.yscale("log")
+plt.ylim(.09,50)
+plt.xlim(0.2,3.5)
+print(data_v3[data_v3['MASS']>21])
+plt.title("Host Star Mass vs. Exo-Planet Mass",size=18)
+print()
+
+MsSI = 1.989e30
+TransitDepth_List = []
+TransitProb_List = []
+Mplanet = []
+Mhost = []
+HabitablePlanets = []
+for i in range(len(data_v3["NAME"])):
+  Mh = data.iloc[i]["MSTAR"]*MsSI
+  if Mh<1.66*MsSI:
+    Rh=1.06*Rs*pow(Mh/MsSI,0.945)
+  else:
+    Rh=1.33*Rs*pow(Mh/MsSI,0.555)
+
+  Mp = data_v3.iloc[i]["MASS"]*1.898e27
+  Ap = data_v3.iloc[i]["A"]*1.496e+11
+  Ep = data_v3.iloc[i]["ECC"]
+  Rp = pow(Mp/MsSI,0.485)*Rs
+  TransitDepth_List.append(TransitDepth(Rp,Rs))
+  TransitProb_List.append(TransitProb(Rh,Ap,Ep))
+  Mplanet.append(Mp/9.223e18)
+  Mhost.append(Mh/9.223e18)
+  
+plt.scatter(data_v3["MSTAR"],data_v3["MASS"],alpha=.4,s=13)
+plt.xlabel("Host Star Mass (M☉)",size=15)
+plt.ylabel("Exo-Planet Mass (M♃)",size=15)
+plt.yscale("log")
+plt.ylim(.09,50)
+plt.xlim(0.2,3.5)
+print(data_v3[data_v3['MASS']>21])
+plt.title("Host Star Mass vs. Exo-Planet Mass",size=18)
+print()
+
+HabitablePlanetsMSTAR = []
+HabitablePlanetsMASS = []
+Pass = []
+for i in range(len(data_v3["NAME"])):
+  Mh = data_v3.iloc[i]["MSTAR"]
+  Sma = data_v3.iloc[i]["A"]
+  Lh = HostStarLuminosity(Mh)
+  HsystemC = HabitalZoneC(Lh)
+  HsystemF = HabitalZoneF(Lh)
+  Mass = data_v3.iloc[i]["MASS"]
+  Pass.append(Mass)
+  if Sma > HsystemC and Sma < HsystemF:
+    HabitablePlanetsMSTAR.append(data_v3.iloc[i]["MSTAR"])
+    HabitablePlanetsMASS.append(data_v3.iloc[i]["MASS"])
+HabitablePlanetsMASS = np.array(HabitablePlanetsMASS)
+
+plt.scatter(HabitablePlanetsMSTAR,HabitablePlanetsMASS,alpha=.4,s=13)
+plt.xlabel("Host Star Mass (M☉)",size=15)
+plt.ylabel("Exo-Planet Mass (M♃)",size=15)
+plt.yscale("log")
+plt.ylim(.09,50)
+plt.xlim(0.2,3.5)
+print(data_v3[data_v3['MASS']>21])
+plt.title("Host Star Mass vs. Exo-Planet Mass",size=18)
+print()
+
+plt.figure(figsize=(15, 10))
+plt.scatter(data_v3["MSTAR"],data_v3["MASS"], alpha=.9,c=data_v3["A"],cmap=plt.cm.get_cmap('RdPu_r'), s=data_v3["MASS"]*30)
+cb = plt.colorbar()
+cb.set_label(label='Semi-Major Axis (au)',fontsize=20)
+plt.scatter(HabitablePlanetsMSTAR, HabitablePlanetsMASS,s=HabitablePlanetsMASS*30,c='black')
+plt.xlabel("Host Star Mass (M☉)",fontsize=15)
+plt.ylabel("Exo-Planet Mass (M♃)",size=15)
+plt.plot([0, 3.5], [1, 1], 'k-', lw=1.5)
+plt.yscale("log")
+plt.ylim(.09,50)
+plt.xlim(0.2,3.5)
+print(data_v3[data_v3['MASS']>21])
+plt.title("Host Star Mass vs. Exo-Planet Mass",size=18)
+print()
+
+plt.figure(figsize=(15, 10))
+plt.scatter(data_v3["MSTAR"],data_v3["MASS"], alpha=.9,c=data_v3["A"],cmap=plt.cm.get_cmap('RdPu_r'), s=data_v3["MASS"]*30)
+cb = plt.colorbar()
+cb.set_label(label='Semi-Major Axis (au)',fontsize=20)
+plt.scatter(HabitablePlanetsMSTAR, HabitablePlanetsMASS,s=HabitablePlanetsMASS*30,c='black')
+plt.xlabel("Host Star Mass (M☉)",fontsize=15)
+plt.ylabel("Exo-Planet Mass (M♃)",size=15)
+plt.plot([0, 3.5], [1, 1], 'k-', lw=1.5)
+plt.yscale("log")
+plt.ylim(.09,50)
+plt.xlim(0.2,3.5)
+print(data_v3[data_v3['MASS']>21])
+plt.title("Host Star Mass vs. Exo-Planet Mass",size=18)
+print()
